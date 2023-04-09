3378d018c626080b9952ee7af21551ed1f0240e2
Arshia <arshia.shahidi@gmail.com>
2023-04-09 03:34:11 +0300

    README.md updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["www.si.com"],
+    domains: ["www.si.com", "fashionista.com"],
     remotePatterns: [
       {
         protocol: "https",
